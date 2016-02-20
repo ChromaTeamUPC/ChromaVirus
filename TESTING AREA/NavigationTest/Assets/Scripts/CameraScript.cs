13b398796628f3d37b8b48d363c87bde7bf5fbcd
jordisimon <jordisu@gmail.com>
2016-02-20 09:42:51 +0100

    Navigation test
    Some sort of scene created.
    Basic shape moves and rotates with XBox controller.
    If falls: game over
    If reaches red square: win
    
    Ugly and messy code, just testing. Needs cleaning and reoganization

---
+++ TESTING AREA/NavigationTest/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+    public Transform target;
+    public float smoothing = 5f;
+
+    Vector3 offset;
+    // Use this for initialization
+    void Start () {
+        offset = transform.position - target.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 targetCamPos = target.position + offset;
+        targetCamPos.y = transform.position.y; //Y Locked
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+    }
+}
---
+++ TESTING AREA/NavigationTest/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MainMenuController : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //if(Input.GetButtonDown("joystickbutton7"))
+        if (Input.GetKeyDown(KeyCode.JoystickButton7))
+        {
+            SceneManager.LoadScene("MainScene");
+        }
+	}
+}
