81faf7ca87f5610cdf40a83f564c2213083dd242
candiajo <jordicanodiaz@gmail.com>
2016-03-14 00:17:37 +0100

    Intro camera added
    Added an animated camera at the begining of the level.

---
+++ TESTING AREA/NavigationTest2/Assets/Scripts/Camera/EntryCameraScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EntryCameraScript : MonoBehaviour {
+
+    public Camera entryCamera;
+    public Camera mainCamera;
+
+	void ChangeToMainCamera()
+    {
+        mainCamera.transform.position = entryCamera.transform.position;
+        mainCamera.transform.rotation = entryCamera.transform.rotation;
+        entryCamera.enabled = false;
+    }
+}
