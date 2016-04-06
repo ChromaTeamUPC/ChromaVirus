b5c7f1b9f31c7700cfdbbb128ed8c6f66c3db9a6
candiajo <jordicanodiaz@gmail.com>
2016-04-06 20:35:32 +0200

    Enemy Control Project Added
    For testing enemy movement.

---
+++ TESTING AREA/Enemy Control/Assets/Scripts/Action.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Action
+{
+    public const int NEXT_ACTION = -1;
+
+    public enum Type
+    {
+        MOVE,
+        ATTACK
+    }
+
+    public Action(Type n, Vector3 w, float s = 4.0f, int na = NEXT_ACTION)
+    {
+        name = n;
+        waypoint = w;
+        speed = s;
+        nextAction = na;
+    }
+
+    public Type name;
+    public Vector3 waypoint;
+    public int nextAction;
+    public float speed;
+}
---
+++ TESTING AREA/Enemy Control/Assets/Scripts/Enemy1Behaviour.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Enemy1Behaviour : MonoBehaviour {
+
+    public Transform wp1;
+    public Transform wp2;
+    public Transform wp3;
+
+    private List<Action> actionList = new List<Action>();
+    private int currentAction = 0;
+    private NavMeshAgent agent;
+    private bool actionStarted = false;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+        actionList.Add(new Action(Action.Type.MOVE, wp1.position, 15.0f));
+        actionList.Add(new Action(Action.Type.MOVE, wp2.position, 5.0f));
+        actionList.Add(new Action(Action.Type.MOVE, wp3.position, 5.0f, 0));
+    }
+    
+    void Update()
+    {
+        if (currentAction <= actionList.Count)
+        {
+            if (actionList[currentAction].name == Action.Type.MOVE)
+            {
+                if (!actionStarted)
+                {
+                    agent.destination = actionList[currentAction].waypoint;
+                    agent.speed = actionList[currentAction].speed;
+                    actionStarted = true;
+                }
+                else
+                {
+                    if (agent.remainingDistance <= 0.5)
+                    {
+                        if (actionList[currentAction].nextAction == Action.NEXT_ACTION)
+                            currentAction++;
+                        else
+                            currentAction = actionList[currentAction].nextAction;
+
+                        actionStarted = false;
+                    }
+                }
+                
+
+                //agent.destination = actionList[currentAction].waypoint;
+            }
+        }
+    }
+}
