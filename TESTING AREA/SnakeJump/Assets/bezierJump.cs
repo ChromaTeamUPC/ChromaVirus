c9b1650916f5ad28eead74f71ffa32e271e42f99
candiajo <jordicanodiaz@gmail.com>
2016-07-28 19:04:23 +0200

    Added new formula for the snake jump
    A new formula with Bezier curve for better control over the jump, now it
    uses deltaTime also

---
+++ TESTING AREA/SnakeJump/Assets/bezierJump.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bezierJump : MonoBehaviour {
+    public Transform esfera;
+    public int altura = 20;
+    public float duracion = 1;
+
+    Vector3 start;
+    Vector3 control;
+    Vector3 end;
+
+    Vector3 lastPosition;
+
+    float t = 0;
+
+    void Start ()
+    {
+        start = transform.position;
+        end = esfera.position;
+
+        control = (end - start) / 2;
+        control.y = altura;
+
+        lastPosition = start;
+	}
+	
+	void Update ()
+    {
+        // si ha hecho el salto reinicia todo para dar otro
+        if (t >= 1)
+        {
+            t = 0;
+            end = esfera.position;
+
+            control = (end - start) / 2;
+            control.y = altura;
+
+            lastPosition = start;
+        }
+
+        transform.position = Bezier(start, control, end, t);
+        t += (1 / duracion) * Time.deltaTime;
+        transform.LookAt(transform.position + (transform.position - lastPosition));
+
+        lastPosition = transform.position;
+	}
+
+    Vector3 Bezier(Vector3 start_, Vector3 control_, Vector3 end_, float t_)
+    {
+        return (((1 - t_) * (1 - t_)) * start_) + (2 * t_ * (1 - t_) * control_) + ((t_ * t_) * end_);
+    }
+}
+
